272777cc3df1b5b80d42e9a083b7b58e359b452a
xplanet95 <vladis.box@yandex.ru>
2022-12-20 22:01:29 +0800

    created models Category, Tag, Post and made migrations

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название категории')
+    slug = models.SlugField(max_length=255, verbose_name='url категории', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория(ю)'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название тега')
+    slug = models.SlugField(max_length=50, verbose_name='url тега', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название поста')
+    slug = models.SlugField(max_length=255, verbose_name='url поста', unique=True)
+    author = models.CharField(max_length=200, verbose_name='Автор')
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    img = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True, null=True, verbose_name='Изображение')
+    views = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='posts',
+                                 blank=True, null=True, verbose_name='Категория поста')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-created_at']
