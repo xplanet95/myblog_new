23d454ff7bbd7190f55c261d20d9e1dbd0b848b3
xplanet95 <vladis.box@yandex.ru>
2022-12-23 20:18:51 +0800

    footer and header menu sql

---
+++ myblog/blog/templatetags/menu.py
@@ -0,0 +1,14 @@
+from django import template
+from blog.models import Category
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('blog/menu_tpl.html')
+def show_menu(menu_class='menu'):
+    categories = Category.objects.all()  # noqa
+    return {
+        'categories': categories,
+        'menu_class': menu_class,
+    }
