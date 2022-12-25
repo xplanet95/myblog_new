163d260910cd782919151a7a07bfec1dc1414043
xplanet95 <vladis.box@yandex.ru>
2022-12-25 20:16:13 +0800

    active js

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('category/<str:slug>', views.get_category, name='get_category'),
+    path('category/<str:slug>/', views.get_category, name='get_category'),
 ]
