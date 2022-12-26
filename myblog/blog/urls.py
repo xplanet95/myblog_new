b2b5fc3957cc3fb80c78f9fdea476f96500c9c1d
xplanet95 <vladis.box@yandex.ru>
2022-12-26 20:12:01 +0800

    categories pages

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('category/<str:slug>/', views.get_category, name='get_category'),
+    path('category/<str:slug>/', views.PostsByCategory.as_view(), name='get_category'),
     path('post/<str:slug>/', views.get_post, name='post'),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -15,6 +15,21 @@ class Index(ListView):
         return context
 
 
+class PostsByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])  # noqa
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])  # noqa
+        return context
+
+
 def index(request):
     return render(request, 'blog/index.html')
 
