92d02f112bbf2fdb1bd466a98e93a2dc34e590c9
xplanet95 <vladis.box@yandex.ru>
2022-12-26 15:59:58 +0800

    post showe

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
     path('category/<str:slug>/', views.get_category, name='get_category'),
+    path('post/<str:slug>/', views.get_post, name='post'),
 ]
